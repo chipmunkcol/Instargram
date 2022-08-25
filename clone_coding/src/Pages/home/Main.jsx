f6d43a5cbf450d1fc9d417fe804f05573c0888c4
minimi61 <new1573@naver.com>
2022-08-25 13:44:22 +0900

    pagination ing

--- clone_coding/src/Pages/home/Main.jsx
+++ clone_coding/src/Pages/home/Main.jsx
@@ -1,21 +1,15 @@
-import React, { useEffect, useState,useRef} from 'react'
-import styled_components from 'styled-components';
+import React, { useEffect, useState, useRef } from 'react'
+import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
 import {getPost} from '../../Redux/modules/postSlice'
 import PostList from './PostList';
-import OthersModal from './modals/OthersModal';
-import { getUserData } from '../../shared/cookie';
 
 
 function Main() {
   const dispatch = useDispatch();
 
-  // Card modal창
-  // const [othersMenuOpen, setOthersMenuOpen] = React.useState(false);
-  // const [myCardMenuOpen, setmyCardMenuOpen] = React.useState(false);
-
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   
   useEffect(() => { 
     dispatch(getPost())
@@ -39,40 +33,44 @@ function Main() {
   return (
     
     <div>
-      <MainContainer >
-        {/*타인 MUI modal창 구현부분입니다  */}
-        {/* {othersMenuOpen ?
-          <OthersModal othersMenuOpen={othersMenuOpen} setOthersMenuOpen={setOthersMenuOpen} /> :
-          null} */}
-      
+      <MainContainer >      
         {/* 카드 컴포넌트입니다 */}
         {posts.data.content && posts.data.content.map((data, i) => { return <PostList data={data} key={i}  /> })
         }
       </MainContainer>
-      {/* <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-              <span class="sr-only">Previous</span>
-            </a>
-          </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-        <span class="sr-only">Next</span>
-            </a>
-          </li>
-        </ul>
-      </nav> */}
+
+      {/* 페이지네이션입니다 */}
+      <PaginationPosition>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only"></span>
+              </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only"></span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </PaginationPosition>
     </div>
         );
 }
 
-const MainContainer = styled_components.div``
-  
+const MainContainer = styled.div``
+
+const PaginationPosition = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
+`
 
 export default Main
